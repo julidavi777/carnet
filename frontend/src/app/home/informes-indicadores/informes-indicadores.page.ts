9d7b6192161a8653484afc59b807c5baa890ffe3
MariaC30 <103943442+MariaC30@users.noreply.github.com>
2023-04-10 23:07:02 -0500

    Grafica 2 ofertas presentadas

--- frontend/src/app/home/informes-indicadores/informes-indicadores.page.ts
+++ frontend/src/app/home/informes-indicadores/informes-indicadores.page.ts
@@ -7,9 +7,35 @@ import { Chart } from 'chart.js';
   styleUrls: ['./informes-indicadores.page.scss'],
 })
 export class InformesIndicadoresPage implements OnInit {
-  chart: any;
 
-  constructor() { }
+
+  isLarge = false;
+
+toggleSize() {
+this.isLarge = !this.isLarge;
+}
+chart: any;
+showTable = false;
+
+public columnSize: number = 2;
+public isExpanded: boolean = false;
+
+expandColumn(): void {
+if (!this.isExpanded) {
+this.columnSize = 5; // reduce la columna a la mitad del tamaño original
+this.isExpanded = true;
+} else {
+this.columnSize = 3;
+this.isExpanded = false;
+}
+}
+
+constructor() { }
+
+toggleCard() {
+this.isExpanded = !this.isExpanded;
+}
+
 
   ngOnInit(): void {
     const cantCotizacionData = {
@@ -20,21 +46,7 @@ export class InformesIndicadoresPage implements OnInit {
       borderWidth: 1
     };
 
-    // const valorCotizadoData = {
-    //   label: 'Valor Cotizado',
-    //   data: [2426123475, 5532506160, 10137416340, 14933353838, 22724668511, 31826497443, 27767320222, 17616276341, 36097335555, 33270877386],
-    //   backgroundColor: 'rgba(54, 162, 235, 0.2)',
-    //   borderColor: 'rgba(54, 162, 235, 1)',
-    //   borderWidth: 1
-    // };
 
-    // const valorVendidoData = {
-    //   label: 'Valor Vendido',
-    //   data: [338451794, 1375582489, 2301445604, 2740141101, 4468492271, 3869987681, 4764715525, 4470838941, 5182437244, 6624266503],
-    //   backgroundColor: 'rgba(75, 192, 192, 0.2)',
-    //   borderColor: 'rgba(75, 192, 192, 1)',
-    //   borderWidth: 1
-    // };
 
     const data = {
       labels: ['2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021', '2022'],
@@ -54,6 +66,38 @@ export class InformesIndicadoresPage implements OnInit {
     this.chart = new Chart('canvas', {
       type: 'line',
       data: data,
+
+    });
+
+    // codigo grafica 2
+
+    const ofertasPresentadas = {
+      label: 'Ofertas presentadas',
+      data: [42,42,57,31,43,29,23,38,37,32,45,23,442],
+
+    };
+
+
+    const data2 = {
+      labels: ['ENE', 'FEB', 'MAR', 'ABR', 'MAY', 'JUN', 'JUL', 'AGO', 'SEP', 'OCT', 'NOV','DIC'],
+      datasets: [ofertasPresentadas]
+    };
+
+    const options2 = {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true,
+            min: 0,
+            max: 60
+          }
+        }]
+      }
+    };
+
+    this.chart = new Chart('canvas2', {
+      type: 'bar',
+      data: data2,
 
     });
   }
