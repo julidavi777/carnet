760812309ba2233f1cab33f2204c5220b091f0cc
Lean360 <gitlean.360@gmail.com>
2023-04-19 15:33:59 -0500

    se configura navbar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -24,12 +24,12 @@
             </div>
 
             <!-- Settings Dropdown -->
-            @auth
+
                 <div class="hidden sm:flex sm:items-center sm:ml-6">
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
                             <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                                <div>{{ Auth::user()->name }}</div>
+                                <div>{{ Auth::user()->name ?? __('Choose an option') }}</div>
 
                                 <div class="ml-1">
                                     <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
@@ -40,6 +40,7 @@
                         </x-slot>
 
                         <x-slot name="content">
+                            @auth
                             <!-- Authentication -->
                             <form method="POST" action="{{ route('logout') }}">
                                 @csrf
@@ -50,10 +51,16 @@
                                     {{ __('Log Out') }}
                                 </x-dropdown-link>
                             </form>
+                            @else
+                                <x-dropdown-link :href="route('login')">{{ __('Log in')}}</x-dropdown-link>
+
+                                @if (Route::has('register'))
+                                    <x-dropdown-link :href="route('register')">{{ __('Register') }}</x-dropdown-link>
+                                @endif
+                            @endauth
                         </x-slot>
                     </x-dropdown>
                 </div>
-            @endauth
 
             <!-- Hamburger -->
             <div class="-mr-2 flex items-center sm:hidden">
