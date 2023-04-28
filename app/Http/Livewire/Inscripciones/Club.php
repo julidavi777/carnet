930023a8f34b1ffa8272f0b319285ad1c35bbb2d
Lean360 <gitlean.360@gmail.com>
2023-04-28 13:33:57 -0500

    se agrega un componente que llamara una lista de clubs

---
+++ app/Http/Livewire/Inscripciones/Club.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Inscripciones;
+
+use Livewire\Component;
+
+class Club extends Component
+{
+    public function render()
+    {
+        return view('livewire.inscripciones.club');
+    }
+}
---
+++ app/Services/Inscripciones/ClubService.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Services\Inscripciones;
+
+class ClubService 
+{
+
+}
