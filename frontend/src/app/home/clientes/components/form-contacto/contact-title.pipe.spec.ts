c7e389be6b467030bda0a69bde387a9dbe54c974
Neider García <yesid19999@gmail.com>
2023-05-15 20:45:58 -0500

    styles for contact modal on create customer

---
+++ frontend/src/app/home/clientes/components/form-contacto/contact-title.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ContactTitlePipe } from './contact-title.pipe';
+
+describe('ContactTitlePipe', () => {
+  it('create an instance', () => {
+    const pipe = new ContactTitlePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
