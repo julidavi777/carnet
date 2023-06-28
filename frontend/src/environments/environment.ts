6247cc452ef6daaf55d1417ed8eaf7aebaf33bb3
N Yesid Garcia <yesid19999@gmail.com>
2023-06-28 08:36:56 -0500

    server prod api for local

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,8 +4,10 @@
 
 export const environment = {
   production: false,
-  baseUrl: "http://127.0.0.1:8000/api/",
-  storageUrl: "http://127.0.0.1:8000/storage/",
+  /* baseUrl: "http://127.0.0.1:8000/api/",
+  storageUrl: "http://127.0.0.1:8000/storage/", */
+  baseUrl: "http://servidor1.cubikar.com/projects/cubikar/backend/public/api/",
+  storageUrl: "http://servidor1.cubikar.com/projects/cubikar/backend/public/storage/",
 
 };
 
