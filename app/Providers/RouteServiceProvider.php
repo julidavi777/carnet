c20fe2f203b758e300a8457da9a63b152c4f1970
Meph6 <leidergananbueno@gmail.com>
2023-08-10 14:20:00 -0500

    se corrige inicio de sesión

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/inscripciones';
+    public const HOME = '/administrar-jugadores';
 
     /**
      * The controller namespace for the application.
