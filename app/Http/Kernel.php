f54dcfe5a54a3fa652d524d287df8cf7aec7f9c0
Meph6 <leidergananbueno@gmail.com>
2023-08-15 10:45:57 -0500

    se agrega middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'preventHistory' => \App\Http\Middleware\PreventBackHistory::class,
+        'cambiarTorneos' => \App\Http\Middleware\EnsureChangeTorneo::class,
     ];
 }
