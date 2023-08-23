7ac95bfa985e7269c77542797d61318211fc21af
Meph6 <leidergananbueno@gmail.com>
2023-08-22 22:28:48 -0500

    se agrega la tabla de los pagos

--- resources/views/inscripciones/inscribir.blade.php
+++ resources/views/inscripciones/inscribir.blade.php
@@ -44,39 +44,12 @@
                 </div>
 
                 <div class="p-6 bg-white border-b border-gray-200">
+
                     {{-- Tabla para la inscripción de los jugadores y el pago total --}}
                     <x-forms.inscripciones-tabla-jugadores :datos="$data_inscripciones" />
 
                     {{-- Tabla para las inscripciones y que hayan realizado el pago --}}
-
-                    <div class="relative overflow-x-auto">
-                        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                            <caption class="p-5 text-lg font-semibold text-left text-gray-900 bg-white dark:text-white dark:bg-gray-800">
-                                Lista de pagos
-                                <p class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">
-                                    A continuación, se listan las inscripciones que pasaron al pago
-                                    y también el estado en el que se encuentra la misma.
-                                </p>
-                            </caption>
-                            <thead class="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
-                                <th scope="col" class="px-6 py-4">Torneo</th>
-                                <th scope="col" class="px-6 py-4">Responsable</th>
-                                <th scope="col" class="px-6 py-4">Total</th>
-                                <th scope="col" class="px-6 py-4">Estado</th>
-                                <th scope="col" class="px-6 py-4">Valor pagado</th>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td class="px-6 py-4">Yisus</td>
-                                    <td class="px-6 py-4">Heaven</td>
-                                    <td class="px-6 py-4">$ 2.000.000</td>
-                                    <td class="px-6 py-4">mediano</td>
-                                    <td class="px-6 py-4">$ 2.000.000</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-
+                    <x-forms.inscripciones-tabla-pagos />
                 </div>
             </div>
         </div>
