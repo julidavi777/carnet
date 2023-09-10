14dde5c50c2820bfdec32ce4e5c23b739cd0f9da
lean360 <gitlean.360@gmail.com>
2023-09-10 12:34:51 -0500

    se arregla bug del estado de pago

--- resources/views/livewire/lista-pagos-component.blade.php
+++ resources/views/livewire/lista-pagos-component.blade.php
@@ -13,7 +13,7 @@
                 <th scope="col" class="px-6 py-4">Torneo</th>
                 <th scope="col" class="px-6 py-4">ID Pago</th>
                 <th scope="col" class="px-6 py-4">Total</th>
-                <th scope="col" class="px-6 py-4">Estado</th>
+                <th scope="col" class="px-6 py-4">Estado pago</th>
                 <th scope="col" class="px-6 py-4">Valor pagado</th>
                 <th scope="col" class="px-6 py-4">Acción</th>
             </thead>
@@ -27,11 +27,18 @@
                             <td class="px-6 py-4">
     
                                 <div class="flex items-center">
-                                    @if($datos['pago_estado'] !== 'A')
-                                        <div class="h-2.5 w-2.5 rounded-full bg-red-500 mr-2"></div> Pendiente
-                                    @else
-                                        <div class="h-2.5 w-2.5 rounded-full bg-green-500 mr-2"></div> Aprobado
-                                    @endif
+
+                                    @switch($datos['pago_estado'])
+                                        @case('F')
+                                                <div class="h-2.5 w-2.5 rounded-full bg-red-500 mr-2"></div> Pendiente
+                                            @break
+                                        @case('P')
+                                                <div class="h-2.5 w-2.5 rounded-full bg-yellow-500 mr-2"></div> Parcial
+                                            @break
+                                        @case('T')
+                                                <div class="h-2.5 w-2.5 rounded-full bg-green-500 mr-2"></div> Aprobado
+                                            @break
+                                    @endswitch
                                 </div>
                             </td>
                             <td class="px-6 py-4">$ {{ $datos['valor_pagado'] ?? 0 }}</td>
@@ -132,7 +139,7 @@
                         {{-- data-modal-hide="defaultModal" --}}
                         wire:click="closeModal"
                         type="button" class="text-center text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
-                        Cerrar modal
+                        Cerrar
                     </button>
                 </div>
             </div>
